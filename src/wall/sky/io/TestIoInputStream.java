10d5ad4bcc65d235e52e0be859b9b363956feb03
Administrator <www_wanglei111@163.com>
2019-03-19 22:48:49 +0800

    as

---
+++ src/wall/sky/io/TestIoInputStream.java
@@ -0,0 +1,16 @@
+package wall.sky.io;
+
+public class TestIoInputStream {
+    public static void main(String[] args){
+        System.out.println("out io test start!!!");
+        System.out.println("================");
+
+        for(int i=1; i<10; i++){
+            for(int j=1; j<= i; j++){
+                System.out.print(i + "*" + j + "\t");
+            }
+            System.out.println();
+        }
+
+    }
+}
